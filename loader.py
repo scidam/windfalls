1593281bc7b944b4bf1b5886b22ae107d1fd1f0c
Kislov Dmitry <kislov@easydan.com>
2018-12-07 13:54:39 +1000

    Combining tiffs not working

--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 
 import numpy as np
+import pandas as pd
 from functools import lru_cache
 import re
 import os
@@ -12,7 +13,7 @@ from shapely.geometry import Point
 
 aux = ['CJ', 'DJ', 'DK']
 LARGE_VALUE = 500
-COMMON_PATH = './19_09_2017/Scene%s/'
+COMMON_PATH = './Sentinel2/19.09.2017/Scene%s/'
 FILE_PAT = "T55T{}_20170919T010641_B{:02}.jp2_Cnv.tif"
 scenes = [1,2,3]
 layers = range(1, 13)
@@ -74,33 +75,32 @@ def get_data_by_coordinate_np(lats, lons, array, xmin, xres, ymax, yres):
     lat_inds = ((lats - ymax) / yres).astype(np.int16)
     lon_inds = ((lons - xmin) / xres).astype(np.int16)
     lat_ind_max, lon_ind_max = array.shape
+    lat_ind_max -= 1
+    lon_ind_max -= 1
     mask_lat = (lat_inds >= 0) & (lat_inds <= lat_ind_max)
     mask_lon = (lon_inds >= 0) & (lon_inds <= lon_ind_max)
-    latitudes = np.arange(ymax, ymax - lat_ind_max * yres, -yres)
-    longitudes = np.arange(xmin, xmin + lon_ind_max * xres, xres)
-    LA, LO = np.meshgrid(latitudes, longitudes)
-    _res = array[lat_inds[mask_lat], lon_inds[mask_lon]]
-    _LA = LA[lat_inds[mask_lat], lon_inds[mask_lon]]
-    _LO = LO[lat_inds[mask_lat], lon_inds[mask_lon]]
+    full_mask = mask_lat & mask_lon
+    _res = array[lat_inds[full_mask], lon_inds[full_mask]]
     _res[np.abs(_res) > LARGE_VALUE] = np.nan
-    return (array, _LA, _LO)
+    return (_res, lats[full_mask], lons[full_mask])
 
 
 def get_arrays(level):
     result = list()
     for f in DATA_PATTERNS[level - 1]:
         data = gdal.Open(f)
-        print(data.GetGeoTransform())
-        if osr.SpatialReference(data.GetProjection()).IsProjected():
+        if  'T55TDK_20170919T010641_B03.jp2_Cnv.tif' in f:
+            geoinfo = (399960.0, 10.0, 0.0, 5000040.0, 0.0, -10.0)
+        elif 'T55TDJ_20170919T010641_B04.jp2_Cnv.tif' in f:
+            geoinfo = (399960.0, 10.0, 0.0, 4900020.0, 0.0, -10.0)
+        else:
             geoinfo = data.GetGeoTransform()
-    xmin = geoinfo[0]
-    xres = geoinfo[1]
-    ymax = geoinfo[3]
-    yrot = geoinfo[4]
-    xrot = geoinfo[2]
-    yres = geoinfo[-1]
-    for f in DATA_PATTERNS[level - 1]:
-        data = gdal.Open(f)
+        xmin = geoinfo[0]
+        xres = geoinfo[1]
+        ymax = geoinfo[3]
+        yrot = geoinfo[4]
+        xrot = geoinfo[2]
+        yres = geoinfo[-1]
         if not np.isclose(xrot, 0) or not np.isclose(yrot, 0):
             raise BaseException("xrot and yrot should be 0")
         array = data.ReadAsArray()
@@ -109,15 +109,41 @@ def get_arrays(level):
     return result
 
 
+def list_all_info():
+    '''Auxiliry function: it doesn't used in the computational process'''
+    for f in sum(DATA_PATTERNS, []):
+        data = gdal.Open(f)
+        geoinfo = data.GetGeoTransform()
+        print(f, geoinfo)
+
+
 def get_data(lats, lons, levels):
     result = []
     for l in levels:
-        array, xmin, xres, ymax, yres = get_arrays(l)
-        result.append(get_data_by_coordinate_np(np.array(lats, dtype=np.float64),
+        intermediate = []
+        for array, xmin, xres, ymax, yres in get_arrays(l):
+            intermediate.append(get_data_by_coordinate_np(np.array(lats, dtype=np.float64),
                                   np.array(lons, dtype=np.float64),
                                   np.array(array, dtype=np.float64),
                                   xmin, xres, ymax, yres))
+        _a, _b, _c = np.array([]), np.array([]), np.array([])
+        for a, b, c in intermediate:
+            _a = np.append(_a, a)
+            _b = np.append(_b, b)
+            _c = np.append(_c, c)
+        aux = pd.DataFrame({'data': _a, 'lat': _b, 'lon': _c})
+        aux.drop_duplicates(['lat', 'lon'], inplace=True)
+        aux.loc[:, 'special'] = aux.lat.astype(int).apply(str) + aux.lon.astype(int).apply(str)
+        print(aux)
+        aux.sort_values('special', inplace=True, ascending=False)
+        print(aux)
+        aux = aux.iloc[np.argsort(aux.special.values), :]
+        print(aux)
+        print([str(a)+str(b) for a,b in zip(lats, lons)])
+        aux = aux.iloc[np.argsort([str(a)+str(b) for a,b in zip(lats, lons)]),:]
+        result.append([aux.data.values, aux.lat.values, aux.lon.values])
     return result
+
 
 
 
--- tests.py
+++ tests.py
@@ -1,4 +1,19 @@
 from loader import *
+import matplotlib.pyplot as plt
 
-result = get_arrays(4)
-print(result)
+# resultG = get_arrays(2)
+# resultB = get_arrays(3)
+# resultR = get_arrays(4)
+# plt.imshow((resultG[-1][0]+resultB[-1][0]+resultR[-1][0])/3)
+# plt.show()
+
+
+lats = np.arange(4911770, 4911770+300, 60)
+lons = np.arange(450405, 450405+200, 60)
+LA, LO = np.meshgrid(lats, lons)
+print(LA.shape, LO.shape)
+
+result = get_data(LA.ravel(), LO.ravel(), [2,3])
+print(result[0][0].reshape(LA.shape))
+plt.imshow(result[0][0].reshape(LA.shape).T, origin='lower')
+plt.show()
